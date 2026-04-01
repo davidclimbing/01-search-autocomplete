31b398c2d93aba18a92fb1aab66fc2cca3c13bc6
David Oh <general.davidclimbing@gmail.com>
2026-04-01 20:52:28 +0900

    feat: handling enter key

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import { useState } from 'react';
+import { useState, KeyboardEvent } from 'react';
 import './App.css';
 
 import { searchProducts, Product } from './api/products';
 
 function App() {
   const [query, setQuery] = useState('');
-  const [result, setResult] = useState<Product[] | null>(null);
+  const [result, setResult] = useState<Product[]>([]);
   const [isLoading, setLoading] = useState(false);
+
+  const handleKeyDown = async (event: KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      await handleSearch(query)
+    }
+  };
 
   const handleSearch = async (word: string) => {
     setLoading(true);
@@ -14,7 +20,7 @@ function App() {
     try {
       const response = await searchProducts(word)
       setResult(response);
-
+      console.log(response)
     } catch(e) {
       console.error(e)
     } finally {
@@ -31,33 +37,28 @@ function App() {
         <input
           type="text"
           value={query}
+          onKeyDown={handleKeyDown}
           onChange={(e) => setQuery(e.target.value)}
           placeholder="상품을 검색하세요"
           className="search-input"
         />
         <button 
-          className="search-button" 
-          onClick={() => handleSearch(query)}>검색</button>
-        {/* {
-          isLoading ? (
-            <>
-              <div>Loading 중</div>
-            </>
-          ) : (
-            <>
-              <div>
-                {
-                  result ? <>{result}</> : <>검색결과가 없습니다.</>
-                }
-              </div>
-            </>
-          )
-        } */}
+          className="search-button"          
+          onClick={() => handleSearch(query)}>{
+            isLoading ? 
+              <div className='loading'></div> : 
+              <span>검색</span>
+          }
+        </button>
       </div>
-
-
       {/* TODO: 자동완성 목록 구현 */}
+      <div >
+
+      </div>
       {/* TODO: 최근 검색어 구현 */}
+      <div>
+
+      </div>
     </div>
   );
 }
